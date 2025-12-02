b7bb87f7973c4c7bb02e9a6264e9326956975f2f
Lewis <mudaidalewis@gmail.com>
2025-12-02 16:18:19 +0300

    added a registration form

--- app/src/main/java/com/example/coffeecafe/SignupActivity.java
+++ app/src/main/java/com/example/coffeecafe/SignupActivity.java
@@ -1,9 +1,6 @@
 package com.example.coffeecafe;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,28 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-Button registerUser;
+public class SignupActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.signup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        registerUser = findViewById(R.id.bv_register);
-
-        registerUser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent registerIntent = new Intent(MainActivity.this,SignupActivity.class);
-                startActivity(registerIntent);
-            }
-        });
-
     }
 }
